cafad443d5d6e55f76cde0a5b99f9910e9ebdc3f
shashith rashmika <158998995+shashithucsc@users.noreply.github.com>
2026-03-24 12:32:58 +0530

    Comment out HeroSection in Home component
    Comment out HeroSection component in Home function.

--- app/page.tsx
+++ app/page.tsx
@@ -3,7 +3,7 @@ import HeroSection from "@/components/HeroSection";
 export default function Home() {
   return (
     <main className="bg-slate-950 min-h-screen">
-      <HeroSection />
+      {/* <HeroSection /> */}
     </main>
   );
 }
